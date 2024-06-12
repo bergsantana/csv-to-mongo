19d0d97f1f0c07c014d597c9c5a3ba517a7a9663
berg <wdmsantana@gmail.com>
2024-06-12 02:06:56 -0400

    add field to a existing collection

--- readcsv.ts
+++ readcsv.ts
@@ -1,21 +1,74 @@
+import mongoose, { mongo } from "mongoose"
+require('dotenv').config()
+
+const { Schema } = mongoose
+ 
 const fs = require("fs")
 const { parse } = require("csv-parse")
  
-fs.createReadStream("./cl-data.csv")
-    .pipe(parse({   delimiter: ',', from_line: 2 }))
-    .on("data", (row) => {
-        const obj = {
-            description : row[0] ?? '',
-            directors_board: row[4] ?? '',
-        }
-        console.log(obj)
-    })
-    .on('end', () =>{
-        console.log('Finished')
-    })
-    .on('error', (error) => {
-        console.log(error.message)
-    })
-    .on('close', () => {
-        console.log('Closing')
-    })
+
+const JobProfitCenterSchema = new Schema({
+    company: {
+      type: Schema.Types.ObjectId,
+      ref: 'Company',
+      required: false,
+    },
+    id: {
+      type: Number,
+    },
+    description: {
+      type: String,
+    },
+    directorate: {
+      type: String,
+    }
+})
+  
+
+const ProfitCenter = mongoose.model('JobProfitCenter', JobProfitCenterSchema)
+
+const readCsvAndUpdate = async () => {
+
+    
+    await mongoose.connect(process.env.DB_CONN_BONDY, { dbName: 'matchbox-v2' })
+    
+ 
+    let notfoundCount = 0
+
+    fs.createReadStream("./cl-data.csv")
+        .pipe(parse({   delimiter: ',', from_line: 2 }))
+        .on("data", async (row) => {
+            
+            const obj = {
+                cl: row[0] ?? '',
+                description : `${row[0]} - ${row[1]}`  ?? '',
+                economic_group: row[2] ?? ''
+            }
+ 
+            try{
+                const regularExp  = new RegExp(obj.cl, 'i')
+                const found = await ProfitCenter.findOne({description: regularExp})
+                if (found) {
+                    found.$set('directorate', row[5] ?? '')
+                    console.log('found', found)
+                    await found.save()
+                }
+                if(!found) console.log('not found'), notfoundCount++
+                
+            }catch(err){
+                console.log(err)
+            }
+ 
+
+        })
+        .on('end', () =>{
+            console.log('Finished')
+        })
+        .on('error', (error) => {
+            console.log(error.message)
+        })
+    
+        console.log('Not found documents', notfoundCount)
+}   
+
+readCsvAndUpdate()
